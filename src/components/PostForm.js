cad03e2e9969a52de35b4ee3c250da98bb83222c
tushar-chaudhari <chaudharitushar00@gmail.com>
2019-03-03 00:20:46 +0530

    react-done

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -11,12 +11,28 @@ class PostForm extends Component {
     onChange = (e) => {
         this.setState({[e.target.name]: e.target.value}) 
     }
+    onSubmit = (e) => {
+        e.preventDefault();
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body:JSON.stringify(post)
+        })
+        .then(res=> res.json())
+        .then(data => console.log(data));
+    }
     render() { 
         return ( 
             <div>
                 <h1>Add Post</h1>
                 <hr/>
-                <form>
+                <form onSubmit = {this.onSubmit}>
                     <div>
                         <label>Title :</label>
                         <br />
